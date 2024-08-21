3aaf1a15189da4fea384c903c87654422ac6e665
chidokato <tuan.pn92@gmail.com>
2024-08-21 10:16:04 +0700

    update 2

---
+++ public/admin_asset/js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#changepassword').change(function(){
+        if ($(this).is(":checked")) {
+            $(".pass").removeAttr('disabled');
+        }else{
+            $(".pass").attr('disabled','');
+        }
+    });
+});
