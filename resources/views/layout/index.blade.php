b52f6188447425626054e996609ecaefa5c4abce
chidokato <tuan.pn92@gmail.com>
2024-08-21 13:30:58 +0700

    update 2

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -24,10 +24,10 @@
                     <table class="table table-bordered">
                         <thead>
                             <tr>
-                            <th scope="col">BIN's</th>
-                            <th scope="col">VENDOR</th>
-                            <th scope="col" colspan="2">LEVEL</th>
-                            <th scope="col">REGION</th>
+                                <th scope="col">BIN's</th>
+                                <th scope="col">VENDOR</th>
+                                <th scope="col" colspan="2">LEVEL</th>
+                                <th scope="col">REGION</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -130,30 +130,27 @@
             <div class="row">
                 @if(isset($datas))
                 <div class="col-md-12">
-                    <h3>Results (max 1000): {{ count($datas) }}</h3> <?php $i = count($datas); ?>
+                    <h3>Results (max 1000): {{ count($datas) }}</h3>
                     @if(count($datas)>0)
                     <table class="table table-bordered">
                         <thead>
                             <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">BIN's</th>
-                            <th scope="col">VENDOR</th>
-                            <th scope="col">LEVEL</th>
-                            <th scope="col">RANK</th>
-                            <th scope="col">Countries</th>
+                                <th scope="col">BIN's</th>
+                                <th scope="col">VENDOR</th>
+                                <th scope="col">LEVEL</th>
+                                <th scope="col">RANK</th>
+                                <th scope="col">Countries</th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach($datas as $val)
                             <tr>
-                                <td>{{ $i }}</td>
                                 <td>{{$val->Bin}}</td>
                                 <td>{{$val->Brand}}</td>
                                 <td>{{$val->Level}}</td>
                                 <td>{{$val->Type}}</td>
                                 <td>{{$val->Countries}}</td>
                             </tr>
-                            <?php $i = $i-1; ?>
                             @endforeach
                         </tbody>
                     </table>
