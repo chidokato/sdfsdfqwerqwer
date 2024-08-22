8143e75fdb05af05c276cb3eabd480ff41ca3af0
chidokato <tuan.pn92@gmail.com>
2024-08-22 14:09:20 +0700

    update 3

--- resources/views/layout/index.blade.php
+++ resources/views/layout/index.blade.php
@@ -13,7 +13,6 @@
     <link rel="stylesheet" type="text/css" href="assets/css/custom.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
-
 </head>
 <body>
     <div class="body">
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.index')
+
+@section('title')  @endsection
+@section('description')  @endsection
+@section('robots') index, follow @endsection
+@section('url'){{asset('')}}@endsection
+
+@section('css')
+
+@endsection
+
+@section('content')
+
+
+
+@endsection
